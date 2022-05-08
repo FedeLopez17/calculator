b6bce7246c72263edb3ff1aa0386a2001bc7117a
FedeLopez17 <fedelopez1712@gmail.com>
2022-05-08 20:40:26 -0300

    Make buttons

--- calculator.js
+++ calculator.js
@@ -1,26 +1,21 @@
 const buttonsContainer = document.querySelector(".buttons-container");
 // Create calculator's buttons
-for (let rows = 0; rows < 5; rows++){
+for (let rowsIterator = 0; rowsIterator < 5; rowsIterator++){
     const rowOfButtons = document.createElement("div");
     rowOfButtons.classList.add("row-of-buttons");
     buttonsContainer.appendChild(rowOfButtons);
-    if (rows !== 4){
-        for (let buttons = 0; buttons < 4; buttons++){
-            const button = document.createElement("div");
-            button.classList.add("button");
-            button.setAttribute("id", `${rows}${buttons}`);
-            rowOfButtons.appendChild(button);
+    const VALUES = {00:"AC", 01:"C", 02:"%", 03:"÷", 10:"7", 11:"8", 12:"9", 13:"+", 20:"4", 21:"5", 22:"6", 23:"-", 30:"1", 31:"2", 32:"3", 33:"x", 40:"0", 41:".", 42:"="};
+    for (let buttonsIterator = 0; buttonsIterator < 4; buttonsIterator++){
+        const button = document.createElement("div");
+        button.classList.add("button");
+        button.innerHTML = "<p>" + VALUES[Number(`${rowsIterator}${buttonsIterator}`)] + "</p>";
+        button.setAttribute("id", `${rowsIterator}${buttonsIterator}`);
+        if (rowsIterator === 4 && buttonsIterator === 2){
+            button.classList.add("wider-button");
+            buttonsIterator++;
         }
-    }
-    else{ 
-        for(let buttons = 0; buttons < 3; buttons++){
-            const button = document.createElement("div");
-            button.classList.add("button");
-            if (buttons === 2) button.classList.add("wider-button");
-            button.setAttribute("id", `${rows}${buttons}`);
-            rowOfButtons.appendChild(button);
-        }
-    }
+        rowOfButtons.appendChild(button);
+    } 
 }
 
 
